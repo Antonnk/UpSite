6a38db0b459fb6e397e6198cc308ffe31d7d4a57
Anton Kristensen <antonnk96@gmail.com>
2020-06-10 23:19:07 +0200

    Fixed input fields in login and register views

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <x-form action="{{ action([\App\User\Controllers\LoginController::class, 'login']) }}">
-        <x-input class="mb-4" type="email" :label="__('Email')" name="email" :placeholder="__('E-Mail Address')" autofocus></x-input>
-        <x-input class="mb-4" type="password" :label="__('Password')" name="password" :placeholder="__('Password')"></x-input>
+        <x-input :label="__('Email')" name="email" class="mb-4" type="email" :placeholder="__('E-Mail Address')" autofocus></x-input>
+        <x-input :label="__('Password')" name="password" class="mb-4" type="password" :placeholder="__('Password')"></x-input>
         <x-button>{{ __('Login') }}</x-button>
     </x-form>
 @endsection
--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <x-form action="{{ action([\App\User\Controllers\RegisterController::class, 'register']) }}">
-        <x-input class="mb-4" type="email" :label="__('Email')" name="email" :placeholder="__('E-Mail Address')" autofocus></x-input>
-        <x-input class="mb-4" :label="__('First name')" name="first_name" :placeholder="__('Given name')"></x-input>
-        <x-input class="mb-4" :label="__('Last name')" name="last_name" :placeholder="__('Family name')"></x-input>
-        <x-input class="mb-4" type="password" :label="__('Password')" name="password" :placeholder="__('Password')"></x-input>
-        <x-input class="mb-4" type="password" :label="__('Confirm password')" name="password_confirmation" :placeholder="__('Confirm password')"></x-input>
+        <x-input :label="__('Email')" name="email" type="email" class="mb-4" :placeholder="__('E-Mail Address')" autofocus></x-input>
+        <x-input :label="__('First name')" name="first_name" class="mb-4" :placeholder="__('Given name')"></x-input>
+        <x-input :label="__('Last name')" name="last_name" class="mb-4" :placeholder="__('Family name')"></x-input>
+        <x-input :label="__('Password')" class="mb-4" name="password" type="password" :placeholder="__('Password')"></x-input>
+        <x-input :label="__('Confirm password')" name="password_confirmation" class="mb-4"  type="password" :placeholder="__('Confirm password')"></x-input>
         <x-button>{{ __('Register') }}</x-button>
     </x-form>
 @endsection
